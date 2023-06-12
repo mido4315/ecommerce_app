f88ec1356c8303e697c560ff5f06550f09b2d563
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-12 16:30:45 +0300

    create project structure

---
+++ lib/core/utils/app_router.dart
@@ -0,0 +1,31 @@
+import 'package:go_router/go_router.dart';
+
+import '../../features/auth/presentation/views/login_view.dart';
+
+
+abstract class AppRouter {
+  static const kLoginView = '/LoginView';
+  static const kRegisterView = '/BookDetailsView';
+  static const kHomeView = '/SearchView';
+
+  static final router = GoRouter(
+    routes: [
+      // GoRoute(
+      //   path: '/',
+      //   builder: (context, state) => const SplashView(),
+      // ),
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const LoginView(),
+      ),
+      // GoRoute(
+      //   path: kBookDetailsView,
+      //   builder: (context, state) => const BookDetailsView(),
+      // ),
+      // GoRoute(
+      //   path: kSearchView,
+      //   builder: (context, state) => const SearchView(),
+      // ),
+    ],
+  );
+}
