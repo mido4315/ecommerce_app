3053c85a76b1b2645b93a5919b3158d771f8a1b0
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-14 16:14:32 +0300

    create custom_auth_button.dart

---
+++ lib/features/auth/presentation/views/widgets/custom_auth_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CustomAuthButton extends StatelessWidget {
+  final Function()? onTap;
+  final String buttonText;
+
+  const CustomAuthButton({super.key, required this.onTap, required this.buttonText});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(25),
+        margin: const EdgeInsets.symmetric(horizontal: 25),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Center(
+          child: Text(
+            buttonText,
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
