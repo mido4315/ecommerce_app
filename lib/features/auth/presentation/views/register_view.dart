42664c2d0acbc0812236ac7402697a08129fe6fb
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-14 22:22:02 +0300

    add bloc listener to register_view.dart

--- lib/features/auth/presentation/views/register_view.dart
+++ lib/features/auth/presentation/views/register_view.dart
@@ -1,9 +1,12 @@
 import 'package:ecommerce_app/core/utils/app_assets.dart';
 import 'package:ecommerce_app/core/utils/app_router.dart';
 import 'package:ecommerce_app/core/utils/app_styles.dart';
+import 'package:ecommerce_app/core/widgets/custom_snack_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
+import '../view model/register cubit/register_cubit.dart';
 import 'widgets/custom_auth_button.dart';
 import 'widgets/custom_text_form_field.dart';
 import 'widgets/square_tile.dart';
@@ -40,7 +43,14 @@ class _RegisterViewState extends State<RegisterView> {
 
   // sign user in method
   void registerNewUser() {
-    if (_registerKey.currentState!.validate()) {}
+    if (_registerKey.currentState!.validate()) {
+      BlocProvider.of<RegisterCubit>(context).userRegister(
+        name: _nameController.text,
+        email: _emailController.text,
+        password: _passwordController.text,
+        phoneNumber: _phoneNumberController.text,
+      );
+    }
   }
 
   @override
@@ -48,142 +58,151 @@ class _RegisterViewState extends State<RegisterView> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.grey[300],
-      body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // logo
-                Icon(
-                  Icons.lock,
-                  size: 90.r,
-                ),
+      body: BlocListener<RegisterCubit, RegisterState>(
+        listener: (context, state) {
+          if (state is RegisterFailure) {
+            customSnackBar(context, state.errorMessage);
+          }else if(state is RegisterSuccess){
+            customSnackBar(context, 'done ya kabeeer');
+          }
+        },
+        child: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // logo
+                  Icon(
+                    Icons.lock,
+                    size: 90.r,
+                  ),
 
-                SizedBox(height: 20.h),
+                  SizedBox(height: 20.h),
 
-                // welcome back, you've been missed!
-                Text(
-                  'Welcome back you\'ve been missed!',
-                  style: AppStyles.textStyle18,
-                ),
+                  // welcome back, you've been missed!
+                  Text(
+                    'Welcome back you\'ve been missed!',
+                    style: AppStyles.textStyle18,
+                  ),
 
-                SizedBox(height: 25.h),
+                  SizedBox(height: 25.h),
 
-                // name text field
-                Form(
-                  key: _registerKey,
-                  child: Column(
+                  // name text field
+                  Form(
+                    key: _registerKey,
+                    child: Column(
+                      children: [
+                        CustomTextFormField(
+                          controller: _nameController,
+                          hintText: 'Name',
+                          obscureText: false,
+                        ),
+
+                        SizedBox(height: 10.h),
+
+                        // password text field
+                        CustomTextFormField(
+                          controller: _emailController,
+                          hintText: 'Email',
+                          obscureText: false,
+                        ),
+
+                        SizedBox(height: 10.h),
+                        // email text field
+                        CustomTextFormField(
+                          controller: _passwordController,
+                          hintText: 'Password',
+                          obscureText: true,
+                        ),
+
+                        SizedBox(height: 10.h),
+                        CustomTextFormField(
+                          controller: _phoneNumberController,
+                          hintText: 'Phone number',
+                          obscureText: false,
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  SizedBox(height: 10.h),
+
+                  // sign in button
+                  CustomAuthButton(
+                    onTap: registerNewUser,
+                    buttonText: 'Sign Up',
+                  ),
+
+                  SizedBox(height: 10.h),
+                  // or continue with
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 25.0.w),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            thickness: 0.5,
+                            color: Colors.grey[400],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10.0.w),
+                          child: Text(
+                            'Or continue with',
+                            style: AppStyles.textStyle18,
+                          ),
+                        ),
+                        Expanded(
+                          child: Divider(
+                            thickness: 0.5,
+                            color: Colors.grey[400],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  SizedBox(height: 10.h),
+
+                  // google + apple sign in buttons
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      CustomTextFormField(
-                        controller: _nameController,
-                        hintText: 'Name',
-                        obscureText: false,
-                      ),
+                      // google button
+                      const SquareTile(imagePath: AppAssets.googleLogo),
 
-                      SizedBox(height: 10.h),
+                      SizedBox(width: 25.w),
 
-                      // password text field
-                      CustomTextFormField(
-                        controller: _emailController,
-                        hintText: 'Email',
-                        obscureText: false,
-                      ),
-
-                      SizedBox(height: 10.h),
-                      // email text field
-                      CustomTextFormField(
-                        controller: _passwordController,
-                        hintText: 'Password',
-                        obscureText: true,
-                      ),
-
-                      SizedBox(height: 10.h),
-                      CustomTextFormField(
-                        controller: _phoneNumberController,
-                        hintText: 'Phone number',
-                        obscureText: false,
-                      ),
+                      // apple button
+                      const SquareTile(imagePath: AppAssets.appleLogo)
                     ],
                   ),
-                ),
 
-                SizedBox(height: 10.h),
+                  SizedBox(height: 10.h),
 
-                // sign in button
-                CustomAuthButton(
-                  onTap: registerNewUser,
-                  buttonText: 'Sign Up',
-                ),
-
-                SizedBox(height: 10.h),
-                // or continue with
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 25.0.w),
-                  child: Row(
+                  // You are a member? Login now
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Expanded(
-                        child: Divider(
-                          thickness: 0.5,
-                          color: Colors.grey[400],
-                        ),
+                      Text(
+                        'You are a member?',
+                        style: AppStyles.textStyle18,
                       ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 10.0.w),
+                      SizedBox(width: 4.w),
+                      TextButton(
+                        onPressed: () {
+                          context.go(AppRouter.kLoginView);
+                        },
                         child: Text(
-                          'Or continue with',
-                          style: AppStyles.textStyle18,
-                        ),
-                      ),
-                      Expanded(
-                        child: Divider(
-                          thickness: 0.5,
-                          color: Colors.grey[400],
+                          'Login',
+                          style: AppStyles.textStyleBlue,
                         ),
                       ),
                     ],
-                  ),
-                ),
-
-                SizedBox(height: 10.h),
-
-                // google + apple sign in buttons
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    // google button
-                    const SquareTile(imagePath: AppAssets.googleLogo),
-
-                    SizedBox(width: 25.w),
-
-                    // apple button
-                    const SquareTile(imagePath: AppAssets.appleLogo)
-                  ],
-                ),
-
-                SizedBox(height: 10.h),
-
-                // not a member? register now
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'You are a member?',
-                      style: AppStyles.textStyle18,
-                    ),
-                    SizedBox(width: 4.w),
-                    TextButton(
-                      onPressed: () {
-                        context.go(AppRouter.kLoginView);
-                      },
-                      child: Text(
-                        'Login',
-                        style: AppStyles.textStyleBlue,
-                      ),
-                    ),
-                  ],
-                )
-              ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
