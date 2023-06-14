af295b9339b5ea7cb17f51eb39fc3a40b774333c
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-14 16:12:50 +0300

    create custom_text_form_field.dart

---
+++ lib/features/auth/presentation/views/widgets/custom_text_form_field.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final bool obscureText;
+
+  const CustomTextFormField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: TextFormField(
+        validator: (val) {
+          if ( val == null || val.isEmpty) {
+            return '$hintText is not valid';
+          }
+          return null;
+        },
+        onChanged: (value) {
+        },
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+            enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey.shade400),
+            ),
+            fillColor: Colors.grey.shade200,
+            filled: true,
+            hintText: hintText,
+            hintStyle: TextStyle(color: Colors.grey[500])),
+      ),
+    );
+  }
+}
