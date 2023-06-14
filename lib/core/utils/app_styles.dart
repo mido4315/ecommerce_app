9952c078a61d98c105f13e6396599ba55033d297
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-14 16:11:58 +0300

    create styles

---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static TextStyle textStyle18 = TextStyle(
+    color: Colors.grey[700],
+    fontSize: 16,
+  );
+  static TextStyle textStyleBlue = const TextStyle(
+    color: Colors.blue,
+    fontWeight: FontWeight.bold,
+  );
+}
