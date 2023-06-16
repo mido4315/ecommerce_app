7680e4ac235ff2003bc803025028be4c09414a02
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-16 03:45:13 +0300

    add SharedPrefs singleton service_locator.dart

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -1,11 +1,17 @@
 import 'package:dio/dio.dart';
 import 'package:ecommerce_app/features/auth/data/repos/auth_repo_impl.dart';
 import 'package:get_it/get_it.dart';
+
 import 'api_service.dart';
+import 'shared_preferences.dart';
 
 final getIt = GetIt.instance;
 
 void setupServiceLocator() {
+  getIt.registerSingleton<SharedPrefs>(
+    SharedPrefs(),
+    signalsReady: true,
+  );
   getIt.registerSingleton<APIService>(
     APIService(
       Dio(),
