28b40f9a89618f678aabd1159ab19173825f2f93
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-16 03:44:11 +0300

    user_provider.dart

---
+++ lib/core/utils/user_provider.dart
@@ -0,0 +1,16 @@
+// import 'package:flutter/material.dart';
+//
+// import '../../features/auth/data/models/user model/user_model.dart';
+//
+//
+// class UserProvider extends ChangeNotifier {
+//
+//    UserModel _user = UserModel(email: '', password: '',id: '',address: '',name: '',token: '',type: '');
+//
+//    UserModel get user => _user;
+//
+//    void setUser(String user) {
+//       _user = UserModel.fromJson(user);
+//       notifyListeners();
+//    }
+// }
