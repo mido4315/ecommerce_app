b1fed931a99f5e0786db8098c5db22a3cd3e00d1
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-16 03:41:00 +0300

    update

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -3,31 +3,52 @@ import 'package:dio/dio.dart';
 class APIService {
   APIService(this._dio);
 
-  final String _baseUrl = 'http://10.0.2.2:6969';
+  final String _baseUrl = 'http://10.0.2.2:3020';
   final Map<String, dynamic> _headers = {
     'Content-Type': 'application/json; charset=utf-8',
   };
 
   final Dio _dio;
 
-  Future<Map<String, dynamic>> get({required String path}) async {
+  Future<Map<String, dynamic>> get({
+    required String path,
+    Map<String, dynamic>? headers,
+  }) async {
     Response response = await _dio.get(
       '$_baseUrl$path',
+      options: Options(
+        headers: headers,
+      ),
     );
     return response.data;
   }
 
   Future<Map<String, dynamic>> post({
+    Map<String, dynamic>? headers,
     required String path,
-    required String data,
+    String? data,
   }) async {
     Response response = await _dio.post(
       '$_baseUrl$path',
       options: Options(
-        headers: _headers,
+        headers: headers ?? _headers,
       ),
       data: data,
     );
     return response.data;
+  }
+
+  Future isValidToken({
+    required Map<String, dynamic> headers,
+    String? data,
+  }) async {
+    Response response = await _dio.post(
+      '$_baseUrl/isValidToken',
+      options: Options(
+        headers: headers,
+      ),
+      data: data,
+    );
+    return response;
   }
 }
