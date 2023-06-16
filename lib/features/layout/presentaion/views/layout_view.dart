e96ccc625d092fb54b299b680545d73aa898261a
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-16 21:03:40 +0300

    update layout_view.dart

--- lib/features/layout/presentaion/views/layout_view.dart
+++ lib/features/layout/presentaion/views/layout_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../view model/layout_cubit/layout_cubit.dart';
+import 'widgets/custom_bottom_navigation_bar.dart';
 
 class LayoutView extends StatelessWidget {
   const LayoutView({Key? key}) : super(key: key);
@@ -12,15 +13,10 @@ class LayoutView extends StatelessWidget {
     return BlocBuilder<LayoutCubit, LayoutState>(
       builder: (context, state) {
         return Scaffold(
-          bottomNavigationBar: BottomNavigationBar(
-              selectedItemColor: Colors.blue,
-              unselectedItemColor: Colors.red,
-              type: BottomNavigationBarType.shifting,
-              currentIndex: cubit.bottomNavigationBarCurrentIndex,
-              onTap: (value) {
-                cubit.changeTap(value);
-              },
-              items: cubit.bottomNavigationBarItems,
+          bottomNavigationBar: CustomBottomNavigationBar(
+            cubit: cubit,
+            listOfIcons: cubit.listOfIcons,
+            listOfStrings: cubit.listOfStrings,
           ),
           body: SafeArea(
             child: cubit.layoutPages[cubit.bottomNavigationBarCurrentIndex],
