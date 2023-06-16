1f67cccacd35c720206a3e492bb11e913c3b0842
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-16 03:42:15 +0300

    update auth_repo.dart and add getUserData

--- lib/features/auth/data/repos/auth_repo.dart
+++ lib/features/auth/data/repos/auth_repo.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:ecommerce_app/features/auth/data/models/user%20model/user_model.dart';
+
 import '../../../../core/errors/failure.dart';
 
 abstract class AuthRepo {
@@ -7,7 +8,10 @@ abstract class AuthRepo {
     required String name,
     required String email,
     required String password,
-    required String phoneNumber,
 });
-  Future<Either<Failure, UserModel>> userLogin();
+  Future<Either<Failure, UserModel>> userLogin({
+    required String email,
+    required String password,
+});
+  Future<void> getUserData();
 }
