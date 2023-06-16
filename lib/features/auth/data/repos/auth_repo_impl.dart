1b40eef4fe9ae7ea30e92399630cd02b6c0043da
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-16 03:42:39 +0300

    update auth_repo_impl.dart and add getUserData

--- lib/features/auth/data/repos/auth_repo_impl.dart
+++ lib/features/auth/data/repos/auth_repo_impl.dart
@@ -1,9 +1,10 @@
+
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
-
 import 'package:ecommerce_app/core/errors/failure.dart';
 import 'package:ecommerce_app/core/utils/api_service.dart';
-
+import 'package:ecommerce_app/core/utils/service_locator.dart';
+import 'package:ecommerce_app/core/utils/shared_preferences.dart';
 import 'package:ecommerce_app/features/auth/data/models/user%20model/user_model.dart';
 
 import 'auth_repo.dart';
@@ -12,23 +13,24 @@ class AuthRepoImpl implements AuthRepo {
   AuthRepoImpl(this.apiService);
 
   APIService apiService;
+  late UserModel myUserModel;
 
   @override
   Future<Either<Failure, UserModel>> userRegister({
     required String name,
     required String email,
     required String password,
-    required String phoneNumber,
   }) async {
     try {
       UserModel data = UserModel(
         name: name,
         email: email,
         password: password,
-        phoneNumber: phoneNumber,
       );
-      var result = await apiService.post(path: '/api/signup', data: data.toJson());
+      var result =
+          await apiService.post(path: '/api/signup', data: data.toJson());
       UserModel user = UserModel.fromMap(result);
+      myUserModel = user;
       return Right(user);
     } catch (e) {
       if (e is DioException) {
@@ -43,8 +45,64 @@ class AuthRepoImpl implements AuthRepo {
   }
 
   @override
-  Future<Either<Failure, UserModel>> userLogin() {
-    // TODO: implement userLogin
-    throw UnimplementedError();
+  Future<Either<Failure, UserModel>> userLogin({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      UserModel data = UserModel(
+        email: email,
+        password: password,
+      );
+      var result =
+          await apiService.post(path: '/api/signin', data: data.toJson());
+      UserModel user = UserModel.fromMap(result);
+      getIt.get<SharedPrefs>().setData(key: 'token', value: user.token);
+
+      //______________________________________saving user data with shard preferences
+      // List<String> userArgs = [
+      //   user.name!,
+      //   user.email,
+      //   user.address!,
+      //   user.type!,
+      //   user.token!,
+      // ];
+      // getIt.get<SharedPrefs>().setData(key: 'user', value: userArgs);
+      myUserModel = user;
+      return Right(user);
+    } catch (e) {
+      if (e is DioException) {
+        return Left(
+          ServerFailure.fromDioError(e),
+        );
+      }
+      return left(ServerFailure(
+        e.toString(),
+      ));
+    }
+  }
+
+//______________________________________get user data every time the app opens and check his token
+  @override
+  Future<void> getUserData() async {
+    try {
+      String token = getIt.get<SharedPrefs>().getData(key: 'token');
+      final Map<String, String> headers = {
+        'Content-Type': 'application/json; charset=utf-8',
+        'my-souq-auth-token': token
+      };
+      Response result = await apiService.isValidToken(
+        headers: headers,
+      );
+      if (result.data == true) {
+        var userResult = await apiService.get(
+          path: "/",
+          headers: headers,
+        );
+        myUserModel = UserModel.fromMap(userResult);
+      }
+    } catch (e) {
+      print(e);
+    }
   }
 }
