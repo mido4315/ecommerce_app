ab7136a7261c939ded130bf9c25eea32907ab651
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-19 01:14:57 +0300

    update custom_text_form_field.dart

--- lib/features/auth/presentation/views/widgets/custom_text_form_field.dart
+++ lib/features/auth/presentation/views/widgets/custom_text_form_field.dart
@@ -18,26 +18,25 @@ class CustomTextFormField extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 25.0),
       child: TextFormField(
         validator: (val) {
-          if ( val == null || val.isEmpty) {
+          if (val == null || val.isEmpty) {
             return '$hintText is not valid';
           }
           return null;
         },
-        onChanged: (value) {
-        },
         controller: controller,
         obscureText: obscureText,
         decoration: InputDecoration(
-            enabledBorder: const OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.white),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.grey.shade400),
-            ),
-            fillColor: Colors.grey.shade200,
-            filled: true,
-            hintText: hintText,
-            hintStyle: TextStyle(color: Colors.grey[500])),
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey.shade400),
+          ),
+          fillColor: Colors.grey.shade200,
+          filled: true,
+          hintText: hintText,
+          hintStyle: TextStyle(color: Colors.grey[500]),
+        ),
       ),
     );
   }
