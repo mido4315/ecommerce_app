62dbc719a6b8424831faa541c9c749ce9e3cf470
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-19 01:12:20 +0300

    add text styles to app styles

---
+++ lib/core/constants/app_styles.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+
+class AppStyles {
+  static TextStyle textStyle18 = TextStyle(
+    color: Colors.grey[700],
+    fontSize: 16,
+  );
+  static TextStyle textStyleTitle20 = const TextStyle(
+    color: AppColors.text2,
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+  static TextStyle textStyleTitle30 = const TextStyle(
+    color: AppColors.text1,
+    fontSize: 30,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle textStyleBlue = const TextStyle(
+    color: Colors.blue,
+    fontWeight: FontWeight.bold,
+  );
+}
