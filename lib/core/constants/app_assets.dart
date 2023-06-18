fec0903651e2253c1e294d35768b740bfece1b8d
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-19 01:11:40 +0300

    add testImages to app_assets.dart

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,4 +1,11 @@
 class AppAssets {
   static const String appleLogo = 'assets/images/apple.png';
   static const String googleLogo = 'assets/images/google.png';
+  static const List<String> testImages = [
+    'https://m.media-amazon.com/images/I/61sCd6EWexL._SX3000_.jpg',
+    'https://m.media-amazon.com/images/I/61lLCq8wlPL._SX3000_.jpg',
+    'https://m.media-amazon.com/images/I/71ncyN6TByL._SX3000_.jpg',
+    'https://m.media-amazon.com/images/I/71yMGkX8M9L._SX3000_.jpg',
+    'https://m.media-amazon.com/images/I/71LH0z75dSL._SX3000_.jpg',
+  ];
 }
