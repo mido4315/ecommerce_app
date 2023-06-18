3eaa438ebf544cf7835370b2dc089787b6cac9ba
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-19 01:18:06 +0300

    add search and slider to home view

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,4 +1,8 @@
+
 import 'package:flutter/material.dart';
+
+import 'widgets/custom_images_slider.dart';
+import 'widgets/home_search_bar.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -8,9 +12,12 @@ class HomeView extends StatelessWidget {
     return const Scaffold(
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-
-          ]
+            HomeSearchBar(),
+            CustomImagesSlider(),
+          ],
         ),
       ),
     );
