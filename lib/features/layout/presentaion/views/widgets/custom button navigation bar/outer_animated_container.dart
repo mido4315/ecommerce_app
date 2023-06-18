eb90619eea69878a1d06719e4944a07776d51a31
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-19 01:16:13 +0300

    create outer_animated_container.dart

---
+++ lib/features/layout/presentaion/views/widgets/custom button navigation bar/outer_animated_container.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../core/constants/app_colors.dart';
+
+class OuterAnimatedContainer extends StatelessWidget {
+  const OuterAnimatedContainer({
+    super.key,
+    required this.bottomNavigationBarCurrentIndex,
+    required this.displayWidth,
+    required this.index,
+  });
+
+  final int bottomNavigationBarCurrentIndex;
+  final double displayWidth;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(seconds: 1),
+      curve: Curves.fastLinearToSlowEaseIn,
+      width: index == bottomNavigationBarCurrentIndex
+          ? .32 * displayWidth
+          : .18 * displayWidth,
+      alignment: Alignment.center,
+      child: AnimatedContainer(
+        duration: const Duration(seconds: 1),
+        curve: Curves.fastLinearToSlowEaseIn,
+        height:
+            index == bottomNavigationBarCurrentIndex ? .12 * displayWidth : 0,
+        width:
+            index == bottomNavigationBarCurrentIndex ? .32 * displayWidth : 0,
+        decoration: BoxDecoration(
+          color: index == bottomNavigationBarCurrentIndex
+              ? AppColors.card
+              : Colors.transparent,
+          borderRadius: BorderRadius.circular(50),
+        ),
+      ),
+    );
+  }
+}
