4d1e8b09fd24aa77d04450dd4bedd94f5c6a092f
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-19 03:58:32 +0300

    create admin_cubit.dart

---
+++ lib/features/admin/admin panel/presentation/view model/admin cubit/admin_cubit.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'admin_state.dart';
+
+class AdminCubit extends Cubit<AdminState> {
+  AdminCubit() : super(AdminInitial());
+
+
+  int bottomNavigationBarCurrentIndex = 1;
+
+  void changeTap(newIndex) {
+    bottomNavigationBarCurrentIndex = newIndex;
+    emit(AdminChangeTap());
+  }
+
+
+  final List<IconData> listOfIcons = [
+    Icons.post_add,
+    Icons.analytics,
+    Icons.all_inbox_rounded,
+  ];
+
+  final List<String> listOfStrings = [
+    'Posts',
+    'Analytics',
+    'Items',
+  ];
+  final List<Widget> layoutPages = [
+    const Center(
+      child: Text(
+        'Posts Page',
+        style: TextStyle(fontSize: 40),
+      ),
+    ),
+    const Center(
+      child: Text(
+        'Analytics Page',
+        style: TextStyle(fontSize: 40),
+      ),
+    ),
+    const Center(
+      child: Text(
+        'Items Page',
+        style: TextStyle(fontSize: 40),
+      ),
+    ),
+  ];
+}
