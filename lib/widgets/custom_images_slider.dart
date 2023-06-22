9fac3dc08dbe4880a9c5d5e49c3d402525fae400
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-22 09:27:56 +0300

    change custom_images_slider.dart

--- lib/widgets/custom_images_slider.dart
+++ lib/widgets/custom_images_slider.dart
@@ -1,21 +1,34 @@
+import 'dart:io';
+
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../../core/constants/app_assets.dart';
+import '../core/constants/app_assets.dart';
 
 class CustomImagesSlider extends StatelessWidget {
   const CustomImagesSlider({
     super.key,
+    required this.isFile,
+    this.images,
   });
+
+  final List<File>? images;
+  final bool isFile;
 
   @override
   Widget build(BuildContext context) {
     return CarouselSlider(
-      items: AppAssets.testImages.map((e) {
-        return Builder(builder: (context) {
-          return Image.network(e);
-        });
-      }).toList(),
+      items: isFile
+          ? images!.map((e) {
+              return Builder(builder: (context) {
+                return Image.file(e);
+              });
+            }).toList()
+          : AppAssets.testImages.map((e) {
+              return Builder(builder: (context) {
+                return Image.network(e);
+              });
+            }).toList(),
       options: CarouselOptions(
         viewportFraction: 0.9,
         initialPage: 0,
