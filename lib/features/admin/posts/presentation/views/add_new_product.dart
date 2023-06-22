59186a8d32400cde63fb5861275b9071886339cb
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-22 12:18:55 +0300

    use bloc listener to add_new_product.dart

--- lib/features/admin/posts/presentation/views/add_new_product.dart
+++ lib/features/admin/posts/presentation/views/add_new_product.dart
@@ -1,12 +1,14 @@
 import 'dart:io';
-
 import 'package:ecommerce_app/core/constants/app_styles.dart';
 import 'package:ecommerce_app/core/widgets/pick_images.dart';
 import 'package:ecommerce_app/widgets/custom_images_slider.dart';
 import 'package:ecommerce_app/widgets/custom_text_form_field.dart';
 import 'package:ecommerce_app/widgets/simpe_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../../../core/widgets/custom_snack_bar.dart';
+import '../view model/add new product cubit/add_new_product_cubit.dart';
 import 'widgets/custom_dotted_box.dart';
 
 class AddNewProduct extends StatefulWidget {
@@ -58,108 +60,129 @@ class _AddNewProductState extends State<AddNewProduct> {
     'Cables and Connectors',
     'Power Supplies',
   ];
-  String? selected = 'Hard Drives';
+  String selected = 'Hard Drives';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: simpleAppBar(title: 'Add New Product'),
-      body: SingleChildScrollView(
-        child: Form(
-          key: _addNewProductKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              images.isNotEmpty
-                  ? SizedBox(
-                width: double.infinity,
-                height: 165.h,
-                    child: Padding(
-                      padding: const EdgeInsets.only(bottom: 20.0),
-                      child: CustomImagesSlider(
-                          isFile: true,
-                          images: images,
+    return BlocListener<AddNewProductCubit, AddNewProductState>(
+      listener: (context, state) {
+        if (state is AddNewProductFailure) {
+          customSnackBar(context, state.errorMessage);
+        } else if (state is AddNewProductSuccess) {
+          customSnackBar(context, 'Product added successfully');
+        }
+      },
+      child: Scaffold(
+        appBar: simpleAppBar(title: 'Add New Product'),
+        body: SingleChildScrollView(
+          child: Form(
+            key: _addNewProductKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                images.isNotEmpty
+                    ? SizedBox(
+                        width: double.infinity,
+                        height: 165.h,
+                        child: Padding(
+                          padding: const EdgeInsets.only(bottom: 20.0),
+                          child: CustomImagesSlider(
+                            isFile: true,
+                            images: images,
+                          ),
                         ),
-                    ),
-                  )
-                  : CustomDottedBox(
-                      onTap: pickProductImage,
-                    ),
-              CustomTextFormField(
-                controller: _nameController,
-                hintText: 'Name',
-                obscureText: false,
-              ),
-              SizedBox(height: 15.h),
-              CustomTextFormField(
-                controller: _descriptionController,
-                hintText: 'Description',
-                obscureText: false,
-                maxLines: 2,
-              ),
-              SizedBox(height: 15.h),
-              CustomTextFormField(
-                controller: _priceController,
-                hintText: 'Price',
-                obscureText: false,
-              ),
-              SizedBox(height: 15.h),
-              CustomTextFormField(
-                controller: _quantityController,
-                hintText: 'quantity',
-                obscureText: false,
-              ),
-              SizedBox(height: 15.h),
-              Padding(
-                padding:
-                    const EdgeInsets.only(left: 25.0, right: 25, bottom: 18),
-                child: Card(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Category',
-                        style: AppStyles.textStyleTitle22,
+                      )
+                    : CustomDottedBox(
+                        onTap: pickProductImage,
                       ),
-                      DropdownButton(
-                        alignment: Alignment.center,
-                        borderRadius: BorderRadius.circular(20),
-                        icon: const Icon(Icons.keyboard_arrow_down),
-                        items: categories.map((e) {
-                          return DropdownMenuItem(
-                            value: e,
-                            alignment: Alignment.center,
-                            child: Text(
-                              e,
-                              textAlign: TextAlign.center,
-                            ),
-                          );
-                        }).toList(),
-                        value: selected,
-                        onChanged: (value) {
-                          setState(() {
-                            selected = value;
-                          });
-                        },
-                      ),
-                    ],
+                CustomTextFormField(
+                  controller: _nameController,
+                  hintText: 'Name',
+                  obscureText: false,
+                ),
+                SizedBox(height: 15.h),
+                CustomTextFormField(
+                  controller: _descriptionController,
+                  hintText: 'Description',
+                  obscureText: false,
+                  maxLines: 2,
+                ),
+                SizedBox(height: 15.h),
+                CustomTextFormField(
+                  controller: _priceController,
+                  hintText: 'Price',
+                  obscureText: false,
+                ),
+                SizedBox(height: 15.h),
+                CustomTextFormField(
+                  controller: _quantityController,
+                  hintText: 'quantity',
+                  obscureText: false,
+                ),
+                SizedBox(height: 15.h),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(left: 25.0, right: 25, bottom: 18),
+                  child: Card(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Category',
+                          style: AppStyles.textStyleTitle22,
+                        ),
+                        DropdownButton(
+                          alignment: Alignment.center,
+                          borderRadius: BorderRadius.circular(20),
+                          icon: const Icon(Icons.keyboard_arrow_down),
+                          items: categories.map((e) {
+                            return DropdownMenuItem(
+                              value: e,
+                              alignment: Alignment.center,
+                              child: Text(
+                                e,
+                                textAlign: TextAlign.center,
+                              ),
+                            );
+                          }).toList(),
+                          value: selected,
+                          onChanged: (value) {
+                            setState(() {
+                              selected = value ?? 'Hard Drives';
+                            });
+                          },
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                width: 300.w,
-                height: 60.h,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text(
-                    'add',
-                    style: AppStyles.textStyleTitle30,
+                SizedBox(
+                  width: 300.w,
+                  height: 60.h,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_addNewProductKey.currentState!.validate() &&
+                          images.isNotEmpty) {
+                        BlocProvider.of<AddNewProductCubit>(context).addProduct(
+                          name: _nameController.text,
+                          description: _descriptionController.text,
+                          price: double.parse(_priceController.text),
+                          quantity: int.parse(_quantityController.text),
+                          category: selected,
+                          images: images,
+                        );
+                      }
+                    },
+                    child: Text(
+                      'add',
+                      style: AppStyles.textStyleTitle30,
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
