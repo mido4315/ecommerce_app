b9173c364ace56639f72077d308ee4a56eafcffd
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-22 12:17:20 +0300

    create add_new_product_state.dart

---
+++ lib/features/admin/posts/presentation/view model/add new product cubit/add_new_product_state.dart
@@ -0,0 +1,11 @@
+part of 'add_new_product_cubit.dart';
+
+abstract class AddNewProductState {}
+
+class AddNewProductInitial extends AddNewProductState {}
+
+class AddNewProductSuccess extends AddNewProductState {}
+class AddNewProductFailure extends AddNewProductState {
+  AddNewProductFailure(this.errorMessage);
+  final String errorMessage;
+}
