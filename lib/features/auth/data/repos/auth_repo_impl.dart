29ff490fa49fad03043895fa836838fa1a19cb73
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-22 13:23:38 +0300

    done

--- lib/features/auth/data/repos/auth_repo_impl.dart
+++ lib/features/auth/data/repos/auth_repo_impl.dart
@@ -6,7 +6,6 @@ import 'package:ecommerce_app/core/utils/api_service.dart';
 import 'package:ecommerce_app/core/utils/service_locator.dart';
 import 'package:ecommerce_app/core/utils/shared_preferences.dart';
 import 'package:ecommerce_app/features/auth/data/models/user%20model/user_model.dart';
-import 'package:flutter/material.dart';
 
 import 'auth_repo.dart';
 
@@ -105,7 +104,7 @@ class AuthRepoImpl implements AuthRepo {
         myUserModel = UserModel.fromMap(userResult);
       }
     } catch (e) {
-      debugPrint(e.toString());
+      print(e);
     }
   }
 }
