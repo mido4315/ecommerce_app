241eeab3eab4f45667cece0cf39eed38e095a0ab
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-22 12:19:57 +0300

    change add product to return product model

--- lib/features/admin/posts/data/repos/posts_repo.dart
+++ lib/features/admin/posts/data/repos/posts_repo.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
 
 import 'package:dartz/dartz.dart';
+import 'package:ecommerce_app/features/admin/posts/data/models/product_model.dart';
 
 import '../../../../../core/errors/failure.dart';
 
 abstract class PostsRepo {
-  Future<Either<Failure, bool>> addProduct({
+  Future<Either<Failure, ProductModel>> addProduct({
     required String name,
     required String description,
     required double price,
