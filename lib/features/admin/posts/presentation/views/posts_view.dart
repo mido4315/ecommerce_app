87b9ed7ec76db71affd27a3fd20d56a3f8d8b33e
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-23 04:14:52 +0300

    add list view to posts view

--- lib/features/admin/posts/presentation/views/posts_view.dart
+++ lib/features/admin/posts/presentation/views/posts_view.dart
@@ -1,6 +1,9 @@
 import 'package:ecommerce_app/core/constants/app_colors.dart';
 import 'package:ecommerce_app/core/utils/app_router.dart';
+import 'package:ecommerce_app/features/admin/posts/presentation/view%20model/posts%20cubit/posts_cubit.dart';
+import 'package:ecommerce_app/features/admin/posts/presentation/views/add_new_product.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import '../../../../../widgets/simpe_app_bar.dart';
 
@@ -13,6 +16,7 @@ class PostsView extends StatelessWidget {
       appBar: simpleAppBar(title: 'Posts'),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          GoRouter.of(context).push(AppRouter.kAddNewProduct);
         },
         backgroundColor: AppColors.buttonLight,
         child: const Icon(
@@ -20,17 +24,31 @@ class PostsView extends StatelessWidget {
           size: 30,
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              context.push(AppRouter.kAddNewProduct);
-            },
-            child: Text('test'),
-          ),
-        ],
+      body: BlocBuilder<PostsCubit, PostsState>(
+        builder: (context, state) {
+          if (state is PostsSuccess) {
+            return ListView.builder(
+              itemCount: state.productsList.length,
+              itemBuilder: (context, index) {
+                return Text(
+                  state.productsList[index].name,
+                  style: const TextStyle(fontSize: 50),
+                );
+              },
+            );
+          } else if (state is PostsFailure) {
+            return Center(
+              child: Text(
+                state.errorMessage,
+                style: const TextStyle(fontSize: 50),
+              ),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
