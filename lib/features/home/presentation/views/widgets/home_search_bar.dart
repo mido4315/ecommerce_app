1c7f6897cabe4fded9cfbec5523fd7f5eab57a0e
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-24 12:41:29 +0300

    use bloc provider for search

--- lib/features/home/presentation/views/widgets/home_search_bar.dart
+++ lib/features/home/presentation/views/widgets/home_search_bar.dart
@@ -1,5 +1,9 @@
-
+import 'package:ecommerce_app/core/utils/app_router.dart';
+import 'package:ecommerce_app/core/widgets/custom_snack_bar.dart';
+import 'package:ecommerce_app/features/home/presentation/view%20model/home%20cubit/home_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../../../core/constants/app_colors.dart';
 
@@ -13,6 +17,9 @@ class HomeSearchBar extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
       child: TextField(
+        onSubmitted: (value) {
+          BlocProvider.of<HomeCubit>(context).getSearchedProducts(searchString: value);
+        },
         decoration: InputDecoration(
           enabledBorder: const OutlineInputBorder(
             borderSide: BorderSide(color: Colors.white),
@@ -25,8 +32,11 @@ class HomeSearchBar extends StatelessWidget {
           fillColor: Colors.white,
           filled: true,
           hintText: 'Search...',
-          hintStyle:  TextStyle(color: AppColors.text1.withOpacity(0.6)),
-          prefixIcon:  Icon(Icons.search,color: AppColors.text1.withOpacity(0.6),),
+          hintStyle: TextStyle(color: AppColors.text1.withOpacity(0.6)),
+          prefixIcon: Icon(
+            Icons.search,
+            color: AppColors.text1.withOpacity(0.6),
+          ),
         ),
       ),
     );
