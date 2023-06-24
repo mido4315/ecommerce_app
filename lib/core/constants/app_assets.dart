5acdbf3765c0ee6ea9facc4cfcfa7d0059ea5de8
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-24 08:34:01 +0300

    add assets as a map

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -8,5 +8,43 @@ class AppAssets {
     'https://m.media-amazon.com/images/I/71yMGkX8M9L._SX3000_.jpg',
     'https://m.media-amazon.com/images/I/71LH0z75dSL._SX3000_.jpg',
   ];
+  static const List<Map<String, String>> categoriesIcons = [
+    {
+      'category': 'Laptop',
+      'icon': 'assets/icons/laptop.png',
+    },
+    {
+      'category': 'Hard Drives',
+      'icon': 'assets/icons/hard-drive-device.png',
+    },
+    {
+      'category': 'RAM',
+      'icon': 'assets/icons/ram-memory.png',
+    },
+    {
+      'category': 'Graphic Cards',
+      'icon': 'assets/icons/graphic-card.png',
+    },
+    {
+      'category': 'Monitor',
+      'icon': 'assets/icons/monitor.png',
+    },
+    {
+      'category': 'SSDs',
+      'icon': 'assets/icons/ssd-drive.png',
+    },
+    {
+      'category': 'Printer',
+      'icon': 'assets/icons/printer.png',
+    },
+    {
+      'category': 'Cables',
+      'icon': 'assets/icons/cable-connector.png',
+    },
+    {
+      'category': 'Power Supplies',
+      'icon': 'assets/icons/power-supply.png',
+    },
+  ];
 
 }
