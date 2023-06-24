3b96de0b6d488ae37a113b3cb308dfabc81b0c7e
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-24 12:38:28 +0300

    create home_repo.dart

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import '../../../../core/errors/failure.dart';
+import '../../../admin/posts/data/models/product_model.dart';
+
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<ProductModel>>> getSearchedProducts({required String searchString});
+
+}
