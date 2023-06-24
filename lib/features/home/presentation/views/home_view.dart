6297941a99705acc5980ce578d45d00d71c301ed
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-24 12:39:18 +0300

    update home_view.dart

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,7 +1,12 @@
 import 'package:ecommerce_app/core/constants/app_assets.dart';
+import 'package:ecommerce_app/features/home/presentation/view%20model/home%20cubit/home_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 
+import '../../../../core/utils/app_router.dart';
+import '../../../../core/widgets/custom_snack_bar.dart';
 import '../../../../widgets/custom_images_slider.dart';
 import 'widgets/categories_bar.dart';
 import 'widgets/home_search_bar.dart';
@@ -11,18 +16,29 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            HomeSearchBar(),
-            CategoriesBar(),
-            CustomImagesSlider(
-              isFile: false,
-            ),
-          ],
+    return BlocListener<HomeCubit, HomeState>(
+      listener: (context, state) {
+        if (state is HomeSuccess) {
+          context.push(AppRouter.kSearchView, extra: state.searchedProducts);
+        } else if (state is HomeFailure) {
+          customSnackBar(context, state.errorMessage);
+        }
+      },
+      child: const Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              HomeSearchBar(),
+              CategoriesBar(),
+              CustomImagesSlider(
+                isFile: false,
+                images: AppAssets.testImages,
+                autoPlay: true,
+              ),
+            ],
+          ),
         ),
       ),
     );
