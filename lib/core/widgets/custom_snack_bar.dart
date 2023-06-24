f0a07fb617138735654d11349e495ca17f3a5366
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-24 08:34:27 +0300

    update custom_snack_bar.dart

--- lib/core/widgets/custom_snack_bar.dart
+++ lib/core/widgets/custom_snack_bar.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void customSnackBar(context, String text) {
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
+      duration: const Duration(milliseconds: 700),
       content: Text(text),
     ),
   );
