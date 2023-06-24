f33951713108ca62dbd5258331cee3ac4ba731d1
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-24 12:38:57 +0300

    create home_cubit.dart

---
+++ lib/features/home/presentation/view model/home cubit/home_cubit.dart
@@ -0,0 +1,26 @@
+import 'package:bloc/bloc.dart';
+import 'package:ecommerce_app/features/home/data/repos/home_repo_impl.dart';
+import 'package:meta/meta.dart';
+
+import '../../../../admin/posts/data/models/product_model.dart';
+
+part 'home_state.dart';
+
+class HomeCubit extends Cubit<HomeState> {
+  HomeCubit(this.homeRepo) : super(HomeInitial());
+
+  final HomeRepoImpl homeRepo;
+
+  Future<void> getSearchedProducts({required String searchString}) async {
+    emit(HomeLoading());
+    var result = await homeRepo.getSearchedProducts(searchString: searchString);
+    result.fold(
+      (failure) => emit(
+        HomeFailure(failure.errorMessage),
+      ),
+      (productsList) => emit(
+        HomeSuccess(productsList),
+      ),
+    );
+  }
+}
