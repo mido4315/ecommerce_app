7c134055ce71fd144e5ae9e696a3a38e107a4350
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-24 12:38:50 +0300

    create home_state.dart

---
+++ lib/features/home/presentation/view model/home cubit/home_state.dart
@@ -0,0 +1,20 @@
+part of 'home_cubit.dart';
+
+@immutable
+abstract class HomeState {}
+
+class HomeInitial extends HomeState {}
+
+class HomeLoading extends HomeState {}
+
+class HomeSuccess extends HomeState {
+  final List<ProductModel> searchedProducts;
+
+  HomeSuccess(this.searchedProducts);
+}
+
+class HomeFailure extends HomeState {
+  final String errorMessage;
+
+  HomeFailure(this.errorMessage);
+}
