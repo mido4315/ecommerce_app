c8f87dc9a0c7bd502a4c6ebd2fc92cb4162439b5
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-24 12:36:35 +0300

    create categories_repo.dart

---
+++ lib/features/categories details/data/repos/categories_repo.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import '../../../../core/errors/failure.dart';
+import '../../../admin/posts/data/models/product_model.dart';
+
+abstract class CategoriesRepo {
+  Future<Either<Failure, List<ProductModel>>> getProducts({required String category});
+
+}
