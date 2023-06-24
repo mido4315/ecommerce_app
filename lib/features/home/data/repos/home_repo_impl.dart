6be6cd58e2e45831a8fc86ce47f28a2156d6ea8d
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-24 12:38:41 +0300

    create home_repo_impl.dart

---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,50 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:ecommerce_app/core/errors/failure.dart';
+import 'package:ecommerce_app/features/admin/posts/data/models/product_model.dart';
+import '../../../../core/utils/api_service.dart';
+import '../../../../core/utils/service_locator.dart';
+import '../../../auth/data/repos/auth_repo_impl.dart';
+import 'home_repo.dart';
+
+class HomeRepoImpl extends HomeRepo {
+  HomeRepoImpl(this.apiService);
+
+  final APIService apiService;
+
+  // get the admin token
+  final String _token = getIt.get<AuthRepoImpl>().myUserModel.token!;
+
+  // create headers
+  Map<String, String> get _headers => {
+        'Content-Type': 'application/json; charset=utf-8',
+        'my-souq-auth-token': _token
+      };
+
+  @override
+  Future<Either<Failure, List<ProductModel>>> getSearchedProducts(
+      {required String searchString}) async {
+    List<ProductModel> products = [];
+    try {
+      // make a post request to the server
+      List<dynamic> result = await apiService.getList(
+        path: '/api/get-products/search/$searchString',
+        headers: _headers,
+      );
+      for (int i = 0; i < result.length; i++) {
+        products.add(ProductModel.fromMap(result[i]));
+      }
+
+      return right(products);
+    } catch (e) {
+      if (e is DioException) {
+        return Left(
+          ServerFailure.fromDioError(e),
+        );
+      }
+      return Left(
+        ServerFailure(e.toString()),
+      );
+    }
+  }
+}
