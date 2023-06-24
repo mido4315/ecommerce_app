eea0c2c1bf703ba7f9f0c52d138f32f67c063ab1
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-24 12:36:21 +0300

    update categories_details_view.dart

--- lib/features/categories details/presentation/views/categories_details_view.dart
+++ lib/features/categories details/presentation/views/categories_details_view.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../widgets/simple_app_bar.dart';
+import '../../../admin/posts/data/models/product_model.dart';
+import '../../../admin/posts/presentation/view model/posts cubit/posts_cubit.dart';
+import '../../../../widgets/single_product.dart';
+import '../view model/categories details cubit/categories_details_cubit.dart';
 
 class CategoriesDetailsView extends StatelessWidget {
   const CategoriesDetailsView({
@@ -10,8 +17,50 @@ class CategoriesDetailsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(categoryTitle),
+      appBar: simpleAppBar(title: categoryTitle),
+      body: BlocBuilder<CategoriesDetailsCubit, CategoriesDetailsState>(
+        builder: (context, state) {
+          if (state is CategoriesDetailsSuccess) {
+            return GridView.builder(
+              itemCount: state.products.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisExtent: 165,
+              ),
+              itemBuilder: (context, index) {
+                ProductModel theProduct = state.products[index];
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SingleProduct(
+                      image: theProduct.images[0],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(theProduct.name),
+                        Text('${theProduct.price}EGP'),
+                      ],
+                    )
+                  ],
+                );
+              },
+            );
+          } else if (state is CategoriesDetailsFailure) {
+            return Center(
+              child: Text(
+                state.errorMessage,
+                style: const TextStyle(fontSize: 50),
+              ),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
