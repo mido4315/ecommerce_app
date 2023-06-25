bbea1bcebf9205733a129d303052be528dfd2e55
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-25 09:37:23 +0300

    create custom_rating_bar.dart

---
+++ lib/features/product details/presentation/views/widgets/custom_rating_bar.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
+class CustomRatingBar extends StatelessWidget {
+  const CustomRatingBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return RatingBar.builder(
+      initialRating: 3,
+      minRating: 1,
+      direction: Axis.horizontal,
+      allowHalfRating: true,
+      itemCount: 5,
+      itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+      itemBuilder: (context, _) => const Icon(
+        Icons.star,
+        color: Colors.amber,
+      ),
+      onRatingUpdate: (rating) {
+        print(rating);
+      },
+    );
+  }
+}
