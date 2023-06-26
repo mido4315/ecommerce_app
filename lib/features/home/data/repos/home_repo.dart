703940c51da2339bd1a5fa1f71a5911ed93b8903
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-26 07:16:22 +0300

    add getBestProducts in home_repo.dart

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,10 +1,13 @@
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/errors/failure.dart';
-import '../../../admin/posts/data/models/product_model.dart';
-
+import '../../../../core/models/product_model.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failure, List<ProductModel>>> getSearchedProducts({required String searchString});
+  Future<Either<Failure, List<ProductModel>>> getSearchedProducts(
+      {required String searchString});
+
+  Future<Either<Failure, List<ProductModel>>> getBestProducts();
+
 
 }
