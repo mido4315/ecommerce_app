8eced8099f6f3c9922df96370c4fc6cc782b8c4e
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-26 07:15:42 +0300

    update home_view.dart

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/core/constants/app_assets.dart';
+import 'package:ecommerce_app/core/constants/app_styles.dart';
 import 'package:ecommerce_app/features/home/presentation/view%20model/home%20cubit/home_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,37 +10,80 @@ import '../../../../core/widgets/custom_snack_bar.dart';
 import '../../../../widgets/custom_images_slider.dart';
 import 'widgets/categories_bar.dart';
 import 'widgets/home_search_bar.dart';
+import 'widgets/product_card_horizontal.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<HomeCubit, HomeState>(
+    return BlocConsumer<HomeCubit, HomeState>(
       listener: (context, state) {
-        if (state is HomeSuccess) {
+        if (state is SearchSuccess) {
           context.push(AppRouter.kSearchView, extra: state.searchedProducts);
-        } else if (state is HomeFailure) {
+        } else if (state is SearchFailure) {
           customSnackBar(context, state.errorMessage);
         }
       },
-      child: const Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              HomeSearchBar(),
-              CategoriesBar(),
-              CustomImagesSlider(
-                isFile: false,
-                images: AppAssets.testImages,
-                autoPlay: true,
+      builder: (context, state) {
+        if (state is HomeSuccess) {
+          return Scaffold(
+            body: Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const HomeSearchBar(),
+                    const CategoriesBar(),
+                    const CustomImagesSlider(
+                      isFile: false,
+                      images: AppAssets.testImages,
+                      autoPlay: true,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 18.0),
+                      child: Text(
+                        'Deal Of The Day',
+                        style: AppStyles.textStyleTitle26,
+                      ),
+                    ),
+                    ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: state.bestProducts.length,
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () {
+                            context.push(
+                              AppRouter.kProductDetailsView,
+                              extra: state.bestProducts[index],
+                            );
+                          },
+                          child: ProductCardHorizontal(
+                            product: state.bestProducts[index],
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ],
-          ),
-        ),
-      ),
+            ),
+          );
+        } else if (state is HomeFailure) {
+          return Center(
+            child: Text(
+              state.errorMessage,
+              style: const TextStyle(fontSize: 50),
+            ),
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
 }
