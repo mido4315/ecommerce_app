326c9dafb3f485dfbed7c3e5a4de03b9e50d7d98
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-26 07:17:30 +0300

    add getBestProducts implementation in home_cubit.dart

--- lib/features/home/presentation/view model/home cubit/home_cubit.dart
+++ lib/features/home/presentation/view model/home cubit/home_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:ecommerce_app/features/home/data/repos/home_repo_impl.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../../../admin/posts/data/models/product_model.dart';
+import '../../../../../core/models/product_model.dart';
 
 part 'home_state.dart';
 
@@ -11,8 +11,20 @@ class HomeCubit extends Cubit<HomeState> {
   final HomeRepoImpl homeRepo;
 
   Future<void> getSearchedProducts({required String searchString}) async {
-    emit(HomeLoading());
     var result = await homeRepo.getSearchedProducts(searchString: searchString);
+    result.fold(
+      (failure) => emit(
+        SearchFailure(failure.errorMessage),
+      ),
+      (productsList) => emit(
+        SearchSuccess(productsList),
+      ),
+    );
+  }
+
+  Future<void> getBestProducts() async {
+    emit(HomeLoading());
+    var result = await homeRepo.getBestProducts();
     result.fold(
       (failure) => emit(
         HomeFailure(failure.errorMessage),
