408fe4c38a99f37c2a5001af178c48151ebf1cc7
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-26 07:12:24 +0300

    create product_details_cubit.dart

---
+++ lib/features/product details/presentation/view model/product details cubit/product_details_cubit.dart
@@ -0,0 +1,53 @@
+import 'package:bloc/bloc.dart';
+import 'package:ecommerce_app/core/models/user_model.dart';
+import 'package:meta/meta.dart';
+
+import '../../../../../core/models/product_model.dart';
+import '../../../../../core/utils/service_locator.dart';
+import '../../../../auth/data/repos/auth_repo_impl.dart';
+import '../../../data/repos/product_details_repo_impl.dart';
+
+part 'product_details_state.dart';
+
+class ProductDetailsCubit extends Cubit<ProductDetailsState> {
+  ProductDetailsCubit(this.productDetailsRepo) : super(ProductDetailsInitial());
+
+  final ProductDetailsRepoImpl productDetailsRepo;
+  double _totalRating = 0;
+  double userRating = 0;
+  double avgRating = 0;
+
+  void calculateRating(ProductModel product) {
+    final UserModel user = getIt.get<AuthRepoImpl>().myUserModel;
+    for (int i = 0; i < product.rating!.length; i++) {
+      _totalRating += product.rating![i].rating;
+      if (product.rating![i].userId == user.id) {
+        userRating = product.rating![i].rating;
+      }
+      if (_totalRating > 0) {
+        avgRating = _totalRating / product.rating!.length;
+      }
+    }
+  }
+
+
+  Future<void> rateProduct({
+    required ProductModel product,
+    required double rating,
+  }) async {
+    var result =
+        await productDetailsRepo.rateProduct(product: product, rating: rating);
+  }
+
+  int quantity = 0;
+
+  Future<void> addProductToCart({
+    required ProductModel product,
+    required int quantity,
+  }) async {
+    var result = await productDetailsRepo.addProductToCart(
+      product: product,
+      quantity: quantity,
+    );
+  }
+}
