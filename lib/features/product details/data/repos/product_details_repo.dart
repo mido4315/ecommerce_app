68ac70a632951673416909729ec86c38d29e1bc4
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-26 07:12:32 +0300

    create product_details_repo.dart

---
+++ lib/features/product details/data/repos/product_details_repo.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:ecommerce_app/core/errors/failure.dart';
+import 'package:ecommerce_app/core/models/product_model.dart';
+
+abstract class ProductDetailsRepo {
+  Future<Either<Failure, void>> rateProduct({
+    required ProductModel product,
+    required double rating,
+  });
+  Future<Either<Failure, void>> addProductToCart({
+    required ProductModel product,
+    required int quantity,
+  });
+}
