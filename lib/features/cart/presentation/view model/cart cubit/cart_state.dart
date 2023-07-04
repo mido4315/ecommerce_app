117886407d75caaa643487edbb811877e7dfb3ab
mido4315 <mohmed.mahmoud3@gmail.com>
2023-07-04 04:00:04 +0300

    add new states to cart_state.dart

--- lib/features/cart/presentation/view model/cart cubit/cart_state.dart
+++ lib/features/cart/presentation/view model/cart cubit/cart_state.dart
@@ -1,7 +1,16 @@
 part of 'cart_cubit.dart';
 
-
 abstract class CartState {}
 
 class CartInitial extends CartState {}
+
 class ShowCartItems extends CartState {}
+
+class CartQuantityChanges extends CartState {}
+
+class OrderHasBeenCreated extends CartState {}
+class OrderHasNotBeenCreated extends CartState {
+  final String errorMessage;
+
+  OrderHasNotBeenCreated(this.errorMessage);
+}
