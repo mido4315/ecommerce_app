37c57d37c97faeec84567e815bd47beafac1203d
mido4315 <mohmed.mahmoud3@gmail.com>
2023-07-07 10:38:51 +0300

    create my_orders_details_view.dart

---
+++ lib/features/my orders/presentation/views/my_orders_details_view.dart
@@ -0,0 +1,118 @@
+import 'package:ecommerce_app/core/models/order.dart';
+import 'package:ecommerce_app/core/utils/service_locator.dart';
+import 'package:ecommerce_app/features/admin/admin%20orders/data/repos/admin_orders_repo.dart';
+import 'package:ecommerce_app/widgets/simple_app_bar.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../core/utils/api_service.dart';
+import '../../../../widgets/order_tracking.dart';
+import '../../../auth/data/repos/auth_repo_impl.dart';
+
+class MyOrdersDetailsView extends StatelessWidget {
+  const MyOrdersDetailsView({Key? key, required this.order}) : super(key: key);
+
+  final OrderModel order;
+
+  @override
+  Widget build(BuildContext context) {
+    final AdminOrdersRepo adminOrdersRepo =
+        AdminOrdersRepo(getIt.get<APIService>());
+    return Scaffold(
+      appBar: simpleAppBar(title: 'Order Details'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black12,
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                        'Date:  ${DateTime.fromMillisecondsSinceEpoch(order.orderedAt)}'),
+                    Text('ID:    ${order.id}'),
+                    Text('Total: \$${order.totalPrice}'),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'Products',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black12,
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    for (int i = 0; i < order.products.length; i++)
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Row(
+                          children: [
+                            Image.network(
+                              order.products[i].images[0],
+                              height: 120,
+                              width: 120,
+                            ),
+                            const SizedBox(width: 5),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    order.products[i].name,
+                                    style: const TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  Text(
+                                    'Quantity: ${order.products[i].selQty!.toInt()}',
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'Tracking Order',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              OrderTracking(
+                order: order,
+                adminOrdersRepo: adminOrdersRepo,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
