4078b9e987c26728ecf10bff1f6ad2d8b8c5b602
mido4315 <mohmed.mahmoud3@gmail.com>
2023-07-07 10:37:31 +0300

    create admin_orders_cubit.dart

--- lib/features/admin/admin panel/presentation/view model/admin cubit/admin_cubit.dart
+++ lib/features/admin/admin panel/presentation/view model/admin cubit/admin_cubit.dart
@@ -1,8 +1,11 @@
+import 'package:ecommerce_app/features/admin/admin%20orders/data/repos/admin_orders_repo.dart';
+import 'package:ecommerce_app/features/admin/admin%20orders/presentation/view%20model/admin_orders_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../../../core/utils/api_service.dart';
 import '../../../../../../core/utils/service_locator.dart';
+import '../../../../admin orders/presentation/views/admin_orders_view.dart';
 import '../../../../posts/data/repos/posts_repo_impl.dart';
 import '../../../../posts/presentation/view model/posts cubit/posts_cubit.dart';
 import '../../../../posts/presentation/views/posts_view.dart';
@@ -12,14 +15,12 @@ part 'admin_state.dart';
 class AdminCubit extends Cubit<AdminState> {
   AdminCubit() : super(AdminInitial());
 
-
   int bottomNavigationBarCurrentIndex = 1;
 
   void changeTap(newIndex) {
     bottomNavigationBarCurrentIndex = newIndex;
     emit(AdminChangeTap());
   }
-
 
   final List<IconData> listOfIcons = [
     Icons.post_add,
@@ -44,11 +45,11 @@ class AdminCubit extends Cubit<AdminState> {
         style: TextStyle(fontSize: 40),
       ),
     ),
-    const Center(
-      child: Text(
-        'Items Page',
-        style: TextStyle(fontSize: 40),
-      ),
+    BlocProvider(
+      create: (context) =>
+          AdminOrdersCubit(AdminOrdersRepo(getIt.get<APIService>()))
+            ..getOrders(),
+      child: const AdminOrdersView(),
     ),
   ];
 }
