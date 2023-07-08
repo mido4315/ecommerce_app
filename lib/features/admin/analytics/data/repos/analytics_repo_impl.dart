8b781dd735d4c2c49bd23cc6ff83d1bd6fc02809
mido4315 <mohmed.mahmoud3@gmail.com>
2023-07-08 04:02:27 +0300

    create analytics_repo_impl.dart

---
+++ lib/features/admin/analytics/data/repos/analytics_repo_impl.dart
@@ -0,0 +1,74 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:ecommerce_app/core/errors/failure.dart';
+import 'package:ecommerce_app/core/utils/api_service.dart';
+import 'package:ecommerce_app/features/admin/analytics/data/models/sales_model.dart';
+import '../../../../../core/utils/service_locator.dart';
+import '../../../../auth/data/repos/auth_repo_impl.dart';
+import 'analytics_repo.dart';
+
+class AnalyticsRepoImpl extends AnalyticsRepo {
+  AnalyticsRepoImpl(this.apiService);
+
+  final APIService apiService;
+
+  // get the admin token
+  final String _token = getIt.get<AuthRepoImpl>().myUserModel.token!;
+
+  // create headers
+  Map<String, String> get _headers => {
+        'Content-Type': 'application/json; charset=utf-8',
+        'my-souq-auth-token': _token
+      };
+
+  @override
+  Future<Either<Failure, Map<String, dynamic>>> getTotalSales() async {
+    List<Sales> sales = [];
+    double totalSales = 0;
+    double totalOrders = 0;
+    double totalProducts = 0;
+    try {
+      var result = await apiService.get(
+        path: '/admin/analytics',
+        headers: _headers,
+      );
+      totalSales = double.parse(result['totalSales'].toString()) ?? 0.0;
+      totalOrders = double.parse(result['totalOrders'].toString()) ?? 0.0;
+      totalProducts = double.parse(result['totalProducts'].toString()) ?? 0.0;
+      sales = [
+        Sales(
+            label: 'Laptops',
+            totalSale: double.parse(result['catMobiles'].toString()) ?? 0.0),
+        Sales(
+            label: 'Rams',
+            totalSale: double.parse(result['catComputers'].toString()) ?? 0.0),
+        Sales(
+            label: 'Cables',
+            totalSale: double.parse(result['catEssentials'].toString()) ?? 0.0),
+        Sales(
+            label: 'GPUs',
+            totalSale: double.parse(result['catAppliances'].toString()) ?? 0.0),
+        Sales(
+            label: 'HDDs',
+            totalSale: double.parse(result['catFashion'].toString()) ?? 0.0),
+      ];
+      return right(
+        {
+          'sales': sales,
+          'totalSales': totalSales,
+          'totalOrders': totalOrders,
+          'totalProducts': totalProducts,
+        },
+      );
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioError(e),
+        );
+      }
+      return left(
+        ServerFailure(e.toString()),
+      );
+    }
+  }
+}
