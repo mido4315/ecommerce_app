2234b78811046a1425c00fa8a84b978111cbdb43
mido4315 <mohmed.mahmoud3@gmail.com>
2023-07-08 04:03:05 +0300

    create analytics_view.dart

---
+++ lib/features/admin/analytics/presentation/views/analytics_view.dart
@@ -0,0 +1,53 @@
+import 'package:buttons_tabbar/buttons_tabbar.dart';
+import 'package:ecommerce_app/features/admin/analytics/presentation/view%20model/analytics_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_toggle_tab/flutter_toggle_tab.dart';
+
+import 'widgets/analytics_column_chart.dart';
+import 'widgets/total_analysis.dart';
+
+class AnalyticView extends StatelessWidget {
+  const AnalyticView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<AnalyticsCubit, AnalyticsState>(
+        builder: (context, state) {
+      if (state is AnalyticsSuccess) {
+        return SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 16),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: TotalAnalysis(
+                    totalSales: state.totalSales.toString(),
+                    totalOrders: state.totalOrders!.toInt().toString(),
+                    totalProducts: state.totalProducts!.toInt().toString(),
+                  ),
+                ),
+              ),
+              AnalyticsColumnChart(
+                sales: state.sales!,
+                totalSales: state.totalSales!,
+              ),
+            ],
+          ),
+        );
+      } else if (state is AnalyticsFailure) {
+        return Center(
+          child: Text(
+            state.errorMessage,
+            style: const TextStyle(fontSize: 50),
+          ),
+        );
+      } else {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      }
+    });
+  }
+}
